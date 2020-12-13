27ee6ee99d3458347a284d3ea898fb173a94323b
Jose Feliz <jfeliz@camsoft.com.do>
2020-12-12 20:34:36 -0500

    Update Converter.cs

--- JETech.DevExtremeCore/Converter.cs
+++ JETech.DevExtremeCore/Converter.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using JETech.NetCoreWeb.Helper;
 
 namespace JETech.DevExtremeCore
 {
+    internal class FilterDevExtremeObjet 
+    {
+        public string Column { get; set; }
+        public string Condiction { get; set; }
+        public string Value { get; set; }
+    }
+
     public class Converter
     {
         public static string FilterToSql(string filter)
@@ -47,46 +55,65 @@ namespace JETech.DevExtremeCore
         }
 
         public static Expression<Func<t, bool>> FilterToExpresion<t>(string filter)
-        {
-            string filterSql = string.Empty;
-
-            var expre = JETech.NetCoreWeb.Helper.ConverterExpression.GetContainsExpression<t>("Name", "jose");
-
-            Expression.Add(expre, expre);
+        {   
+            Expression<Func<t, bool>> expResult = null;
 
             if (!string.IsNullOrEmpty(filter))
             {
-                string[] filterJson = (string[])Newtonsoft.Json.JsonConvert.DeserializeObject(filter);
-
                 string col, exp, value;
-                string condiction = string.Empty;
 
-                for (int i = 0; i < filterJson.Length; i = +3)
+                filter = filter
+                    .Replace(",\"and\",", ",")
+                    .Replace("[","")
+                    .Replace("]","");
+
+                filter = string.Concat("[", filter, "]");
+
+                var filterJson = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(filter);
+                Expression<Func<t, bool>> expItem = null;                
+
+                for (int i = 0; i < filterJson.Length; i +=3)
                 {
                     col = filterJson[i];
                     value = filterJson[i + 2];
                     exp = filterJson[i + 1];
+                    expItem = null;
 
                     switch (exp)
                     {
-                        case "constains":
-                            condiction = $"{col} like '%{value}%'";
+                        case "contains":
+                            expItem = ConverterExpression.GetContainsExpression<t>(col, value);                           
                             break;
                         default:
                             break;
                     }
-                    if (string.IsNullOrEmpty(filterSql))
+                    if (expResult != null && expItem != null)
                     {
-                        filterSql = condiction;
+                        ParameterExpression param = expResult.Parameters[0];
+
+                        if (ReferenceEquals(param, expItem.Parameters[0]))
+                        {
+                            // simple version
+                            expResult = Expression.Lambda<Func<t, bool>>(
+                                Expression.AndAlso(expItem.Body, expResult.Body), param);
+                        }
+                        else 
+                        {
+                            // otherwise, keep expr1 "as is" and invoke expr2
+                            expResult = Expression.Lambda<Func<t, bool>>(
+                                Expression.AndAlso(
+                                    expResult.Body,
+                                    Expression.Invoke(expItem, param)), param);
+                        }          
                     }
-                    else
+                    else if (expItem != null) 
                     {
-                        filterSql = String.Concat(filterSql, " and ", condiction);
+                        expResult = expItem;
                     }
                 }
             }
 
-            return expre;
+            return expResult;
         }
     }
 }
